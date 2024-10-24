bc1ae441b57741cb7671c92e173a4e0ad4a52a17
MathewPTaylor <142932068+MathewPTaylor@users.noreply.github.com>
2024-10-24 17:57:32 +0000

    rordor website stuff + bubble sort efficiency

--- flasktest.py
+++ flasktest.py
@@ -7,5 +7,9 @@ def index():
     return render_template("decay.html")
 
 
+@app.route("/rordor")
+def rordor():
+    return render_template("rordor.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81, debug=True, auto_reload=True)
